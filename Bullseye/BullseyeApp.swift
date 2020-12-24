1c2eececdcd5962da219639069bce434766d421a
Oscar Cristaldo <fabianskier@icloud.com>
2020-12-24 16:04:49 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Oscar Cristaldo on 2020-12-24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
